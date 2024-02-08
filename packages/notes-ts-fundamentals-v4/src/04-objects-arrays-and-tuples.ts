ae9af3cdbe73e19d3e2a8acc6303e46d3ca1dd9b
Nicholas Gomez <vraven029@gmail.com>
2024-02-07 22:06:02 -0500

    explore through lesson 4 and learn the basics of objects, arrays, tuples

--- packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
+++ packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
@@ -1,118 +1,145 @@
 //* Objects
 
+const myCar = {
+  make: 'Toyota',
+  model: 'Corolla',
+  year: 2002,
+}
+
 let car: {
   make: string
   model: string
   year: number
 }
 
-/*
-//? A function that prints info about a car to stdout
-// function printCar(car: {
-//     make: string
-//     model: string
-//     year: number
-// }) {
-//     console.log(`${car.make} ${car.model} (${car.year})`)
-// }
+// this says myCar is of object type car. ?
+car = myCar
 
-// printCar(car)
+// ? A function that prints info about a car to stdout
+function printCar(car: {
+  make: string
+  model: string
+  year: number
+  chargeVoltage?: number // optional property
+}) {
+  let str = `${car.make} ${car.model} (${car.year})`
+  if (typeof car.chargeVoltage !== 'undefined')
+    str += `// ${car.chargeVoltage}v`
+  console.log(str)
+}
 
-/*
+printCar(car)
+
 //* Optional properties
 //? Insert into function printCar
-// let str = `${car.make} ${car.model} (${car.year})`
-// car.chargeVoltage
-// if (typeof car.chargeVoltage !== "undefined")
-//   str += `// ${car.chargeVoltage}v`
 
-/*
-// printCar({ //? original fn works
-//     make: "Honda",
-//     model: "Accord",
-//     year: 2017,
-// })
+printCar({
+  //? original fn works
+  make: 'Honda',
+  model: 'Accord',
+  year: 2017,
+})
 
-// printCar({ //? optional property works too!
-//     make: "Tesla",
-//     model: "Model 3",
-//     year: 2020,
-//     chargeVoltage: 220,
-// })
+printCar({
+  //? optional property works too!
+  make: 'Tesla',
+  model: 'Model 3',
+  year: 2020,
+  chargeVoltage: 220,
+})
 
-/*
 //* Excess property checking
 
-// printCar({
-//     make: "Tesla",
-//     model: "Model 3",
-//     year: 2020,
-//     color: "RED", //? EXTRA PROPERTY
-// })
+printCar({
+  make: 'Tesla',
+  model: 'Model 3',
+  year: 2020,
+  // color: "RED", //? EXTRA PROPERTY WONT WORK
+})
 
-/*
 //* Index signatures
 
 //? Dictionary of phone #s
-// const phones = {
-//     home: { country: "+1", area: "211", number: "652-4515" },
-//     work: { country: "+1", area: "670", number: "752-5856" },
-//     fax: { country: "+1", area: "322", number: "525-4357" },
-// }
-/*
 //? Model as an index signature
-// const phones: {
-//     [k: string]: {
-//         country: string
-//         area: string
-//         number: string
-//     }
-// } = {}
+// with this we are saying that any attribute in this object, will have
+// an object attached to it with the following types?
+// let phones: {
+//   [key: string]: {
+//     country: string
+//     area: string
+//     number: string
+//   }
+// }
+
+let phones: {
+  mobile: {
+    country: string
+    area: string
+    number: string
+  }
+  [key: string]: {
+    country: string
+    area: string
+    number: string
+  }
+}
+
+const myPhones = {
+  home: { country: '+1', area: '211', number: '652-4515' },
+  mobile: { country: '+1', area: '211', number: '652-4515' },
+  work: { country: '+1', area: '670', number: '752-5856' },
+  fax: { country: '+1', area: '322', number: '525-4357' },
+}
+
+phones = myPhones
+
+// common ts conventions
+phones.mobile // use this for KNOWN attributes
+phones['work'] // use this for CUSTOM attributes / dictionary
 
 //*  Array Types
 
-/*
-// const fileExtensions = ["js", "ts"]
-//        ^? string[]
+const fileExtensions = ['js', 'ts'] as string[]
+//  ^? string[]
 
-// const cars = [ //? Let's look at an array of objects
-//     {
-//         make: "Toyota",
-//         model: "Corolla",
-//         year: 2002,
-//     },
-// ]
-
+const cars = [
+  //? Let's look at an array of objects
+  (car = {
+    make: 'Toyota',
+    model: 'Corolla',
+    year: 2002,
+  }),
+]
 
 //* Tuples
-/*
-// let myCar = [
-//     2002,     // Year
-//     "Toyota", // Make
-//     "Corolla" // Model
-// ]
-// const [year, make, model] = myCar //✔️ Destructuring
+
+// not well typed example
+let myOtherCar = [
+  2002, // Year
+  'Toyota', // Make
+  'Corolla', // Model
+]
+const [year, make, model] = myOtherCar //✔️ Destructuring
 
 //? Inference doesn't work very well for tuples
-/*
-// myCar = ["Honda", 2017, "Accord", "Sedan"] //! Wrong convention
-/*
-// let myCar: [number, string, string] = [
-//     2002,
-//     "Toyota",
-//     "Corolla",
-// ]
-// myCar = ["Honda", 2017, "Accord"] //! Wrong convention
-// myCar = [2017, "Honda", "Accord", "Sedan"] //! Too many elements
 
+myOtherCar = ['Honda', 2017, 'Accord', 'Sedan'] //! Wrong convention
+
+let myOtherOtherCar: [number, string, string] = [
+  2002,
+  'Toyota',
+  'Corolla',
+]
+// myOtherOtherCar = ["Honda", 2017, "Accord"] //! Wrong convention
+// myOtherOtherCar = [2017, "Honda", "Accord", "Sedan"] //! Too many elements
 
 //*  `readonly` tuples
-/*
-// const numPair: [number, number] = [4, 5]; //✔️ Valid
-// const numTriplet: [number, number, number] = [7]; //! Invalid
 
-// [101, 102, 103].length //? number[].length
-// numPair.length //? [number, number] length
+const numPair: [number, number] = [4, 5] //✔️ Valid
+// const numTriplet: [number, number, number] = [7] //! Invalid
+
+;[101, 102, 103].length //? number[].length
+numPair.length //? [number, number] length
 
 // numPair.push(6) // [4, 5, 6]
 // numPair.pop() // [4, 5]
